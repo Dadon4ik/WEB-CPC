f023189e6375519f14f1d22c37000f5e6edac44b
Sergey <51193628+Dadon4ik@users.noreply.github.com>
2019-10-01 09:47:06 +0500

    Update cpc3.php

--- cpc3.php
+++ cpc3.php
@@ -1,18 +1,22 @@
 <?php
 $arr = ['a', 'b', 'c'];
 echo var_dump($arr);
+echo "<br>";
 
 //
 echo $arr[0] . $arr[1] . $arr[2];
+echo "<br>";
 
 //
 $arr = ['a', 'b', 'c', 'd'];
 echo $arr[0] . "+" . $arr[1] . " " . $arr[2] . "+" . $arr[3] . ", " . $arr[0] . "-" . $arr[3] . " " . $arr[0] ."-" . $arr[3];
+echo "<br>";
 
 //
 $arr = [2, 5, 3, 9];
 $result = $arr[0] * $arr[1] + $arr[2] * $arr[3];
 echo $result;
+echo "<br>";
 
 //
 $arr2[] = 1;

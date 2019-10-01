33155ed6d2f8c84c0abe11cb1c369aac30aee19b
Sergey <51193628+Dadon4ik@users.noreply.github.com>
2019-10-01 09:46:08 +0500

    Update cpc2.php

--- cpc2.php
+++ cpc2.php
@@ -1,21 +1,25 @@
 <?php
 $text = "Привет, мир!";
 echo $text . " Моя первая программа!";
+echo "<br>";
 
 //
 $text1 = "Привет, ";
 $text2 = "мир!";
 echo $text1 . $text2;
+echo "<br>";
 
 //
 $name = "Unnamed";
 echo "Привет, " . $name;
 $text = "Новый текст";
+echo "<br>";
 
 //
 $age = 13;
 $height = 125;
 echo "Мой возраст: " . $age . "| Мой рост: " . $height;
+echo "<br>";
 
 //
 $text = "abcde";

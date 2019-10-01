1ae23f6630421ed847341245431ca4d6cbaa6265
Sergey <51193628+Dadon4ik@users.noreply.github.com>
2019-10-01 09:41:00 +0500

    Update cpc1. php

--- cpc1.php
+++ cpc1.php
@@ -1,6 +1,7 @@
 <?php
 $a = 3;
 echo $a;
+echo "\";
 
 //
 $a = 10;
@@ -13,12 +14,14 @@ $result = $a * $b;
 echo $result;
 $result = $a / $b;
 echo $result;
+echo "\";
 
 //
 $c = 15;
 $d = 2;
 $result = $c + $d;
 echo $result;
+echo "\";
 
 //
 $a = 10;
@@ -26,6 +29,7 @@ $b = 2;
 $c = 3;
 $result = ($a + $b + $c) / (($a + $b + $c) / 3);
 echo $result;
+echo "\";
 
 //
 $a = 17;
